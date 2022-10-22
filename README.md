2f1c3c097ff09d12d2c1b8ceb64cc483e9b7b60d
Christian Abele <manufaktur@christian-abele.de>
2022-10-22 17:15:48 +0200

    commit v1.0

---
+++ README.md
@@ -0,0 +1,15 @@
+# Description
+
+Copies the selected text in the active Tab when pressing 'Ctrl + Alt + C' and extending the selected text by the url of the tab and the current timestamp
+
+# Example
+
+```
+Christian Abele
+Softwaremanufaktur
+
+https://christian-abele.de/ - accessed at Sat, 22 Oct 2022 15:04:28 GMT
+```
+
+
+# Inspired by https://github.com/mdn/webextensions-examples/tree/master/selection-to-clipboard
---
+++ content-script.js
@@ -0,0 +1,24 @@
+/*
+copy the selected text to clipboard
+*/
+function copySelection() {
+    let selectedText = window.getSelection().toString().trim();
+
+    if (selectedText) {
+        navigator.clipboard.writeText(selectedText + "\n\n" + window.location.href + " - accessed at " + new Date().toUTCString());
+    }
+}
+
+/*
+Add copySelection() as a listener to mouseup events.
+*/
+// document.addEventListener("mouseup", copySelection);
+document.addEventListener("keydown", (event) => {
+  if (event.isComposing || event.keyCode === 229) {
+    return;
+  }
+  if (event.ctrlKey && event.altKey && event.keyCode == 67) {
+    copySelection();
+  }
+  // do something
+});
